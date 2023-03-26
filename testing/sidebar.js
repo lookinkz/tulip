62e092eeb0df6986c1152e8ad0ad51dd7b20e893
lookinkz <work@quana.kz>
2023-03-26 16:22:00 +0600

    first one

---
+++ testing/sidebar.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidebar").classList.add("open");
+  document.querySelector(".openbtn").classList.add("open");
+  document.querySelector(".main").classList.add("open");
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").classList.remove("open");
+  document.querySelector(".openbtn").classList.remove("open");
+  document.querySelector(".main").classList.remove("open");
+}
